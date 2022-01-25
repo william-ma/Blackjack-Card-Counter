5b39b535cffb55eae350ab5e4d770d32503ec1bd
William Jiachun Ma <willma028@gmail.com>
2022-01-26 10:00:06 +1100

    Add skeleton of Deck

---
+++ src/main/java/blackjack/cardcounter/Deck.java
@@ -0,0 +1,12 @@
+package blackjack.cardcounter;
+
+import java.util.List;
+
+public class Deck {
+
+    public static boolean isValidDecks(int numDecks, List<Card> cards) {
+//        TODO: implement
+        return true;
+    }
+
+}
